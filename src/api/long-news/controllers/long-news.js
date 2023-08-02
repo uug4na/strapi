d357fc55c401d65989108786cc53ade1e338ed84
uug4na <uuganbayar@infinite.mn>
2023-08-03 02:13:00 +0800

    last

--- src/api/long-news/controllers/long-news.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * long-news controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::long-news.long-news');
--- src/api/long-news/services/long-news.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * long-news service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::long-news.long-news');
