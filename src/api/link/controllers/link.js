8af900dbd81958ada8fb0b3046fdd9e34d5afa01
Uuganbayar <uug4na@gmail.com>
2023-08-26 20:43:12 +0800

    author name

--- src/api/link/controllers/link.js
+++ src/api/link/controllers/link.js
@@ -5,5 +5,31 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::link.link', ({ strapi }) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::link.link');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
 
-module.exports = createCoreController('api::link.link');
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
--- src/api/news/controllers/news.js
+++ src/api/news/controllers/news.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::news.news', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::news.news');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
 
-module.exports = createCoreController('api::news.news');
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
