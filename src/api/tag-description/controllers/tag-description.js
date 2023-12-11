34e1b100fa2df0b9307e6d1b1b83a61b52324f13
uug4na <uug4na@gmail.com>
2023-12-11 19:30:21 +0800

    lol

---
+++ src/api/tag-description/controllers/tag-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tag-description controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tag-description.tag-description');
---
+++ src/api/tag-description/routes/tag-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tag-description router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tag-description.tag-description');
